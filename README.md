896d0729818ddb5c498b2b957238e36a6ef46c12
valmsmith39a <valmsmith39a@gmail.com>
2021-08-31 22:59:28 -0700

    load iris flower dataset, fit model, predict route

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# fastapi-ml-iris
+## Iris Flowers Classification
+
+#### Dependencies:
+
+- FastAPI
+- uvicorn
+- scikit-learn
+
+#### API
+
+##### POST request
+
+```
+curl -X 'POST'   8000/predict'   -H 'accept: application/json'   -H 'Content-Type: application/json'   -d '{
+  "sepal_length": 5,
+  "sepal_width": 3,
+  "petal_length": 1,
+  "petal_width": 0.2
+}'
+```
+
+##### Response
+
+```
+{"class":"setosa"}
+```
--- main.py
+++ main.py
@@ -1,13 +1,52 @@
 from fastapi import FastAPI
 import uvicorn
+from sklearn.datasets import load_iris
+from sklearn.naive_bayes import GaussianNB
+from pydantic import BaseModel
 
-# Declare FastAPI instance 
+# declare FastAPI instance
 app = FastAPI()
+
+
+class request_body(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
+
+
+# load Iris dataset
+iris = load_iris()
+
+# get features and targets from dataset
+X = iris.data
+Y = iris.target
+
+# fit model on dataset - train model
+clf = GaussianNB()
+clf.fit(X, Y)
+
 
 @app.get("/")
 def main():
-	return {"message": "Welcome to Iris ML Project"}
+    return {"message": "Welcome to Iris ML Project"}
+
 
 @app.get("/{name}")
-def hello_name(name : str):
-	return { "message": f'Welcome to Iris ML Project, {name}'}
+def hello_name(name: str):
+    return {"message": f'Welcome to Iris ML Project, {name}'}
+
+
+@app.post("/predict")
+def predict(data: request_body):
+    test_data = [[
+        data.sepal_length,
+        data.sepal_width,
+        data.petal_length,
+        data.petal_width
+    ]]
+
+    # predict the class of iris flower
+    class_idx = clf.predict(test_data)[0]
+
+    return {"class": iris.target_names[class_idx]}
